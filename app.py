fae8b2721c40cf9a5496c8108d66a0aa71a5db26
Desktop W10 <robertodelprete88@gmail.com>
2021-12-13 01:02:26 +0100

    commit

--- app.py
+++ app.py
@@ -7,5 +7,11 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/showroom')
+def Showroom():
+    return render_template('showroom.html')
+
+
+
 if __name__ == '__main__':
     app.run()
