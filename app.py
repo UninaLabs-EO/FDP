cbc04f356fd57c541a635683b81d455431f4a810
Desktop W10 <robertodelprete88@gmail.com>
2021-12-29 20:44:11 +0100

    commit

--- app.py
+++ app.py
@@ -1,7 +1,43 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect
 from flask.helpers import url_for
+from forms import RegistrationForm, LoginForm
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
+
+from dotenv import load_dotenv
+load_dotenv('var.env' )
+
 
 app = Flask(__name__)
+
+
+app.config['SECRET_KEY'] = 'secret-key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + 'database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    Bookings = db.relationship('Bookings', backref='user', lazy=True)
+
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}')"
+
+
+class Booking(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date_start = db.Column(db.DateTime, nullable=False)
+    date_end = db.Column(db.DateTime, nullable=False)
+    car = db.Column(db.String(20), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Books('{self.date_start}', '{self.date_end}', '{self.car}')"
 
 @app.route('/')
 def index():
@@ -11,6 +47,37 @@ def index():
 def Showroom():
     return render_template('showroom.html')
 
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+
+@app.route('/db/')
+def testdb():
+    try:
+        db.session.query(text('1')).from_statement(text('SELECT 1')).all()
+        return '<h1>It works.</h1>'
+    except Exception as e:
+        # e holds description of the error
+        error_text = "<p>The error:<br>" + str(e) + "</p>"
+        hed = '<h1>Something is broken.</h1>'
+        return hed + error_text
 
 
 if __name__ == '__main__':
